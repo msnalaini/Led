9124ec542c2d50027011a4f4741a05ec7092c683
msnalaini <113190368+msnalaini@users.noreply.github.com>
2022-10-08 00:42:39 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace sreenMagic {
+    /** 
+     * Address LEDs linearly row first
+    */
+    //% blockId=sreenmagicplotat
+    //% block="plot at index"
+    //% index.min=0 index.max=25
+    export function plotAt(index: number): void {
+        index |= 0
+        const x = Math.floor(index / 5);
+        const y = Math.floor(index % 5);
+        led.plot(x, y)
+    }
+}
